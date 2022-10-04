9ef78ecb1c8751c0cf76bd2bb7e7e0e4eb5ea9ec
moxDroid Labs Inc <pritam.world@gmail.com>
2022-10-03 20:18:03 -0400

    React project code added

---
+++ build/precache-manifest.acd6978345ec2150aa9592906edf5e43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0de1fef45543474e4409",
+    "url": "/pritamworld/comp3104_lab_f2022/static/css/main.8997ae4a.chunk.css"
+  },
+  {
+    "revision": "0de1fef45543474e4409",
+    "url": "/pritamworld/comp3104_lab_f2022/static/js/main.0de1fef4.chunk.js"
+  },
+  {
+    "revision": "c0fa479ba36eae9c8d9e",
+    "url": "/pritamworld/comp3104_lab_f2022/static/js/runtime~main.c0fa479b.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/pritamworld/comp3104_lab_f2022/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "ca81ce7f36685e19a6378006832f4a48",
+    "url": "/pritamworld/comp3104_lab_f2022/index.html"
+  }
+];
